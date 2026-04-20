e99c8ec5369a63a40fcda88fcc17eee9b581e245
Michael-Boat-2 <michael.anokyeboateng@gmail.com>
2026-04-20 18:52:24 +0200

    hard_ai_train

--- Assets/Scripts/QLearning/QLearnAgent.cs
+++ Assets/Scripts/QLearning/QLearnAgent.cs
@@ -250,7 +250,7 @@ namespace QLearning
         {
             if (trainingLog.Length > 0)
             {
-                string path = Path.Combine(Application.streamingAssetsPath, "q_learn_hard.csv");
+                string path = Path.Combine(Application.streamingAssetsPath, "q_learn_ai.csv");
                 string header = "Episode,SurvivalTime,TotalReward \n";
                 
                 File.WriteAllText(path, header + trainingLog.ToString());
