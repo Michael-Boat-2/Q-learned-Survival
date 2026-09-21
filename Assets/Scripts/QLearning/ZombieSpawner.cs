a77346ddb928e7b13fcf0fc223728666603fa3da
Michael-Boat-2 <michael.anokyeboateng@gmail.com>
2026-09-21 15:25:14 +0200

    many changes

--- Assets/Scripts/QLearning/ZombieSpawner.cs
+++ Assets/Scripts/QLearning/ZombieSpawner.cs
@@ -8,8 +8,8 @@ namespace QLearning
         
         [SerializeField] private GameObject zombiePrefab;
         [SerializeField] private Transform[] spawnPoints;
-        [SerializeField] private float baseSpawnInterval = 3f;
-        [SerializeField] private int maxZombies = 12;
+        [SerializeField] private float baseSpawnInterval = 2f;
+        [SerializeField] private int maxZombies = 16;
         
         
         [SerializeField] private Vector2 spawnAreaSize = new Vector2(20f, 20f);
